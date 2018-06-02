7e4cf96087effa20ba0176e29e3f21337786157f
Jamie Chappell <jamiechappell@macawk95.dyndns.cern.ch>
2018-06-02 18:23:27 +0200

    Updated to stepwise function.

--- generate_beam.py
+++ generate_beam.py
@@ -149,7 +149,8 @@ if __name__ == '__main__':
 
     if arguments.type == 'stepwise':
 
-        particle_number = generate_beam_env(filename, meanE, dist='stepwise', spread_number='stepwise')
+        particle_number = generate_beam_env(filename, meanE, dist='stepwise',
+                                            spread_percent=spread)
         spread = 'stepwise'
 
     elif arguments.type == 'percentage':
